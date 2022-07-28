f39f12be6ff07724634982700298d3efb8e6fff4
Bo Lu <burmecia@users.noreply.github.com>
2022-07-28 11:42:07 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ rm /secrets.json
 
 # Sync files to bucket
 echo "Syncing bucket $BUCKET ..."
-gsutil -m rsync -r -c -d -x "$INPUT_EXCLUDE" /github/workspace/$INPUT_SOURCE_DIR gs://$INPUT_BUCKET/
+gsutil -m rsync -r -c -d -x "$INPUT_EXCLUDE" /github/workspace/$INPUT_SYNC_DIR gs://$INPUT_BUCKET/$INPUT_SYNC_DIR
 if [ $? -ne 0 ]; then
     echo "Syncing failed"
     exit 1
