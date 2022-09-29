dcc5c5dffbe27d03dc231c4f0593147abec69e42
Brandon Ruhl <bruhl@redventures.com>
2022-09-29 12:32:03 -0700

    --allow-empty

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # Auth
-echo "$INPUT_SECRETS" > /secrets.json
+echo "$INPUT_SECRETS" >/secrets.json
 gcloud auth activate-service-account --key-file=/secrets.json
 rm /secrets.json
 
 # Sync files to bucket
 echo "Syncing bucket $BUCKET ..."
-gsutil -m rsync -r -c -d -x "$INPUT_EXCLUDE" /github/workspace/$INPUT_SYNC_DIR gs://$INPUT_BUCKET/$INPUT_SYNC_DIR
+gsutil -m rsync -r -c -d -x "$INPUT_EXCLUDE" /github/workspace/$INPUT_GITHUB_DIR gs://$INPUT_BUCKET/$INPUT_GCS_DIR
 if [ $? -ne 0 ]; then
     echo "Syncing failed"
     exit 1
